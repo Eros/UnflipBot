74332700e3a260c0625901403756ec1784479d22
Eros <george.marr.cobc@gmail.com>
2018-07-05 21:36:50 +0100

    okay then

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from twitter import *
 import credentials
+
 import twitter_search as ts
 
 
 def main():
-
     twitter = Twitter(auth=OAuth(credentials.ACCESS_KEY, credentials.SECRET_KEY, credentials.CONSUMER_KEY,
                                  credentials.CONSUMER_SECRET))
+    print("Attempted to auth bot")
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == '__main__':
+    main()
